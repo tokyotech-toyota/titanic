78cc1b52fe74d0c08e930ef6cf680e02c3082d24
ryunosuke-yamashita <ryunosuke.yamashita@aol.com>
2021-05-02 07:47:44 +0900

    [modify] Change number of spaces for indentation: 8 -> 4

--- tree.py
+++ tree.py
@@ -17,12 +17,12 @@ test = pd.read_csv(test_path)
 
 # make table of missing percentage
 def kesson_table(df):
-        null_val = df.isnull().sum()
-        percent = 100 * df.isnull().sum()/len(df)
-        kesson_table = pd.concat([null_val, percent], axis=1)
-        kesson_table_ren_columns = kesson_table.rename(
-        columns = {0 : '欠損数', 1 : '%'})
-        return kesson_table_ren_columns
+    null_val = df.isnull().sum()
+    percent = 100 * df.isnull().sum()/len(df)
+    kesson_table = pd.concat([null_val, percent], axis=1)
+    kesson_table_ren_columns = kesson_table.rename(
+    columns = {0 : '欠損数', 1 : '%'})
+    return kesson_table_ren_columns
 
 # check
 print(kesson_table(train))
